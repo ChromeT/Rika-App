03ba89ee7a9cca2d8241e60c3809379aefb33871
ChromeT <mochacimit@gmail.com>
2026-05-11 03:54:28 +0700

    fix: import useRef in EditGoalScreen

--- src/screens/EditGoalScreen.js
+++ src/screens/EditGoalScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity, TextInput, Alert, Modal, ActivityIndicator } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
